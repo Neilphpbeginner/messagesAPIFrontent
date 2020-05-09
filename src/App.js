38ecba4e19578f2f15d4b3e9e241f8a2dea42fa1
Neil Lemmer <lemmer.neil@gmail.com>
2020-05-09 21:23:35 +0200

    push all

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import NavigationDrawer from './components/NavigationDrawer'
+import { withRouter } from 'react-router'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                <NavigationDrawer title="Home Page" />
+            </div>
+        )
+    }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+    counter: state.counter
+})
+
+const mapDispatchToProps = {
+
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
--- src/index.js
+++ src/index.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, applyMiddleware, compose } from 'redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import { createBrowserHistory } from 'history'
+import { RootReducer } from './reducer';
+import { Container } from '@material-ui/core'
+import App from './App';
+import logger from 'redux-logger'
+import ViewMessages from './components/ViewMessages';
+import NewMessages from './components/NewMessages';
+import DeleteMessageId from './components/DeleteMessageId';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import UpdateSubJectById from './components/UpdateSubJectById';
+
+const customHistory = createBrowserHistory();
+
+const middleware = [thunk, logger];
+
+const store = createStore(
+    RootReducer, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <Container>
+            <Router history={customHistory}>
+                <Route exact path='/' component={App} />
+                <Route path='/viewMessages' component={ViewMessages} />
+                <Route path='/addMessage' component={NewMessages} />
+                <Route path='/deleteMessageByID' component={DeleteMessageId} />
+                <Route path='/updateSubjectByID' component={UpdateSubJectById} />
+            </Router>
+        </Container>
+    </Provider>
+
+    , document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
